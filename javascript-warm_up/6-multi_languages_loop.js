d959bca9f62ca31f38457f206febe17339d7e349
GustaveIR <iraguhagustave@gmail.com>
2024-01-21 02:09:16 +0200

    javascript-warm_up

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+// Array of strings
+const languages = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+// Loop through the array and print each line
+for (const language of languages) {
+    console.log(language);
+}
