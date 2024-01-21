86e0dd4cf4f2bdfbebcfde3190203673a72f8a67
GustaveIR <iraguhagustave@gmail.com>
2024-01-21 13:57:05 +0200

    javascript_objects_scopes_closures

---
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+class Rectangle {
+    constructor(w, h) {
+      if (w <= 0 || h <= 0) {
+        // If width or height is not a positive integer, create an empty object
+        return {};
+      }
+      this.width = w;
+      this.height = h;
+    }
+  }
+  
+  module.exports = Rectangle;
+  
