fd5b6329c121eb32600ee8eb4089811938746de3
GustaveIR <iraguhagustave@gmail.com>
2024-01-21 14:12:19 +0200

    javascript_objects_scopes_closures

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,42 +1,33 @@
 #!/usr/bin/node
 
-class Rectangle {
-    constructor(w, h) {
-      if (w > 0 && h > 0) {
-        this.width = w;
-        this.height = h;
-      } else {
-        // Create an empty object if w or h is not a positive integer
-        this.width = undefined;
-        this.height = undefined;
-      }
-    }
-  
-    // Custom toString method
-    toString() {
-      if (this.width !== undefined && this.height !== undefined) {
-        return `Rectangle { width: ${this.width}, height: ${this.height} }`;
-      } else {
-        return 'Rectangle {}';
-      }
-    }
-  }
-  
-  module.exports = Rectangle;
-  
-  // Test cases
-  const r1 = new Rectangle(3, 3);
-  console.log(r1.toString());
-  
-  const r2 = new Rectangle(3, -3);
-  console.log(r2.toString());
-  
-  const r3 = new Rectangle(3);
-  console.log(r3.toString());
-  
-  const r4 = new Rectangle(undefined, 3);
-  console.log(r4.toString());
-  
-  const r5 = new Rectangle();
-  console.log(r5.toString());
-  
+const Rectangle = require('./2-rectangle');
+
+// Test case - Instance width: 3 - height: 3
+const r1 = new Rectangle(3, 3);
+console.log(r1.toString());
+console.log(r1.width);
+console.log(r1.height);
+
+// Test case - Instance width: 3 - height: -3
+const r2 = new Rectangle(3, -3);
+console.log(r2.toString());
+console.log(r2.width);
+console.log(r2.height);
+
+// Test case - Instance width: 3
+const r3 = new Rectangle(3);
+console.log(r3.toString());
+console.log(r3.width);
+console.log(r3.height);
+
+// Test case - Instance width: undefined - height: 3
+const r4 = new Rectangle(undefined, 3);
+console.log(r4.toString());
+console.log(r4.width);
+console.log(r4.height);
+
+// Test case - Instance no argument
+const r5 = new Rectangle();
+console.log(r5.toString());
+console.log(r5.width);
+console.log(r5.height);
