9277e19b950f0b38d00d3a40b296acbe09531f09
GustaveIR <iraguhagustave@gmail.com>
2024-01-21 14:15:02 +0200

    javascript_objects_scopes_closures

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,22 +1,42 @@
 #!/usr/bin/node
-const Rectangle = require('./2-rectangle');
 
-const r1 = new Rectangle(2, 3);
-console.log(r1);
-console.log(r1.width);
-console.log(r1.height);
-
-const r2 = new Rectangle(2, -3);
-console.log(r2);
-console.log(r2.width);
-console.log(r2.height);
-
-const r3 = new Rectangle(2);
-console.log(r3);
-console.log(r3.width);
-console.log(r3.height);
-
-const r4 = new Rectangle(2, 0);
-console.log(r4);
-console.log(r4.width);
-console.log(r4.height);
+class Rectangle {
+    constructor(w, h) {
+      if (w > 0 && h > 0) {
+        this.width = w;
+        this.height = h;
+      } else {
+        // Create an empty object if w or h is not a positive integer
+        this.width = undefined;
+        this.height = undefined;
+      }
+    }
+  
+    // Custom toString method
+    toString() {
+      if (this.width !== undefined && this.height !== undefined) {
+        return `Rectangle { width: ${this.width}, height: ${this.height} }`;
+      } else {
+        return 'Rectangle {}';
+      }
+    }
+  }
+  
+  module.exports = Rectangle;
+  
+  // Test cases
+  const r1 = new Rectangle(3, 3);
+  console.log(r1.toString());
+  
+  const r2 = new Rectangle(3, -3);
+  console.log(r2.toString());
+  
+  const r3 = new Rectangle(3);
+  console.log(r3.toString());
+  
+  const r4 = new Rectangle(undefined, 3);
+  console.log(r4.toString());
+  
+  const r5 = new Rectangle();
+  console.log(r5.toString());
+  
