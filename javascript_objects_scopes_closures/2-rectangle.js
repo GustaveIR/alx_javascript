920d54e1089916dccac0366bebf60f6ae0f08b96
GustaveIR <iraguhagustave@gmail.com>
2024-01-21 14:02:39 +0200

    javascript_objects_scopes_closures

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -2,14 +2,37 @@
 
 class Rectangle {
     constructor(w, h) {
-      if (w <= 0 || h <= 0) {
-        // If width or height is not a positive integer, create an empty object
-        return {};
+      if (w > 0 && h > 0) {
+        this.width = w;
+        this.height = h;
+      } else {
+        // Create an empty object if w or h is not a positive integer
+        this.width = undefined;
+        this.height = undefined;
       }
-      this.width = w;
-      this.height = h;
+    }
+  
+    // Custom toString method
+    toString() {
+      return `Rectangle { width: ${this.width}, height: ${this.height} }`;
     }
   }
   
   module.exports = Rectangle;
+  
+  // Test cases
+  const r1 = new Rectangle(3, 3);
+  console.log(r1.toString());
+  
+  const r2 = new Rectangle(3, -3);
+  console.log(r2.toString());
+  
+  const r3 = new Rectangle(3);
+  console.log(r3.toString());
+  
+  const r4 = new Rectangle(undefined, 3);
+  console.log(r4.toString());
+  
+  const r5 = new Rectangle();
+  console.log(r5.toString());
   
