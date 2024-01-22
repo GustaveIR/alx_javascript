1b9af51198b4e3cd5c1cc8b0a6206addf113acff
GustaveIR <iraguhagustave@gmail.com>
2024-01-22 19:25:58 +0200

    javascript_objects_scopes_closures

--- javascript_objects_scopes_closures/2-main.js
+++ javascript_objects_scopes_closures/2-main.js
@@ -1,30 +1,22 @@
 #!/usr/bin/node
 const Rectangle = require('./2-rectangle');
 
-const r1 = new Rectangle(2, 3);
-console.log(`Correct output - Instance width: ${r1.width} - height: ${r1.height}`);
-console.log('[Got]');
-console.log(`Rectangle { width: ${r1.width}, height: ${r1.height} }`);
+const r1 = new Rectangle(3, -3);
+console.log(r1);
 console.log(r1.width);
 console.log(r1.height);
 
-const r2 = new Rectangle(2, -3);
-console.log('Correct output - Instance {}');
-console.log('[Got]');
-console.log(`Rectangle { width: ${r2.width}, height: ${r2.height} }`);
+const r2 = new Rectangle(3);
+console.log(r2);
 console.log(r2.width);
 console.log(r2.height);
 
-const r3 = new Rectangle(2);
-console.log('Correct output - Instance {}');
-console.log('[Got]');
-console.log(`Rectangle { width: ${r3.width}, height: ${r3.height} }`);
+const r3 = new Rectangle(undefined, 3);
+console.log(r3);
 console.log(r3.width);
 console.log(r3.height);
 
-const r4 = new Rectangle(2, 0);
-console.log('Correct output - Instance {}');
-console.log('[Got]');
-console.log(`Rectangle { width: ${r4.width}, height: ${r4.height} }`);
+const r4 = new Rectangle();
+console.log(r4);
 console.log(r4.width);
 console.log(r4.height);
