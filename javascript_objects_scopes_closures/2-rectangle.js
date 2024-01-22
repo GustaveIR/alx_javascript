5241d422a3cb89e19d8ae9637814fb71c817d2a2
GustaveIR <iraguhagustave@gmail.com>
2024-01-22 19:15:08 +0200

    javascript_objects_scopes_closures

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,13 +1,13 @@
-#!/usr/bin/node
+// 2-rectangle.js
 
 class Rectangle {
     constructor(w, h) {
-      if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
-        this.width = undefined;
-        this.height = undefined;
-      } else {
+      if (w > 0 && h > 0) {
         this.width = w;
         this.height = h;
+      } else {
+        // If w or h is not a positive integer or is equal to 0, create an empty object
+        Object.create(null);
       }
     }
   }
