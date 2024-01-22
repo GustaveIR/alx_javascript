27bd1bd84ee006e6ed64eb7a65a615ad2b0e6207
GustaveIR <iraguhagustave@gmail.com>
2024-01-22 16:45:22 +0200

    javascript_objects_scopes_closures

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -3,11 +3,9 @@
 class Rectangle {
     constructor(w, h) {
       if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
-        // If width or height is not a positive integer, set them to undefined
         this.width = undefined;
         this.height = undefined;
       } else {
-        // Initialize the instance attributes width and height
         this.width = w;
         this.height = h;
       }
