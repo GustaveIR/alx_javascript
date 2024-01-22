06edd45e431f54db81c391499127ac40c70898c8
GustaveIR <iraguhagustave@gmail.com>
2024-01-22 17:12:54 +0200

    javascript_objects_scopes_closures

--- javascript_objects_scopes_closures/2-main.js
+++ javascript_objects_scopes_closures/2-main.js
@@ -2,21 +2,25 @@
 const Rectangle = require('./2-rectangle');
 
 const r1 = new Rectangle(2, 3);
-console.log(r1);
-console.log(r1.w);
-console.log(r1.h);
+console.log(`Correct output - Instance width: ${r1.width} - height: ${r1.height}`);
+console.log(`[Got]\n${JSON.stringify(r1)}`);
+console.log(r1.width);
+console.log(r1.height);
 
 const r2 = new Rectangle(2, -3);
-console.log(r2);
-console.log(r2.w);
-console.log(r2.h);
+console.log('Correct output - Instance {}');
+console.log(`[Got]\n${JSON.stringify(r2)}`);
+console.log(r2.width);
+console.log(r2.height);
 
 const r3 = new Rectangle(2);
-console.log(r3);
-console.log(r3.w);
-console.log(r3.h);
+console.log('Correct output - Instance {}');
+console.log(`[Got]\n${JSON.stringify(r3)}`);
+console.log(r3.width);
+console.log(r3.height);
 
 const r4 = new Rectangle(2, 0);
-console.log(r4);
-console.log(r4.w);
-console.log(r4.h);
+console.log('Correct output - Instance {}');
+console.log(`[Got]\n${JSON.stringify(r4)}`);
+console.log(r4.width);
+console.log(r4.height);
