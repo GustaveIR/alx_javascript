a9b19bb8f29473b55326dbbd74eb92ccc11f060a
GustaveIR <iraguhagustave@gmail.com>
2024-01-22 16:30:25 +0200

    javascript_objects_scopes_closures

--- javascript_objects_scopes_closures/1-rectangle.js
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -5,10 +5,7 @@ class Rectangle {
       if (w > 0 && h > 0) {
         this.width = w;
         this.height = h;
-      } else {
-        // If w or h is not a positive integer, create an empty object
-        return {};
-      }
+      } 
     }
   }
   
