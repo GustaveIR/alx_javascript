e8b863e14d2a6e7bfd9f7392342bd1ec921f81a3
GustaveIR <iraguhagustave@gmail.com>
2024-01-23 13:02:43 +0200

    javascript-network

--- javascript-network/3-request_store.js
+++ javascript-network/3-request_store.js
@@ -4,13 +4,17 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   if (req.url === '/route_0') {
-    // Respond with correct output for small text
-    res.write('Correct output - small text');
-    res.end();
+    // Fetch content from the specified URL for small text
+    fetchContent('http://loripsum.net/api', (content) => {
+      res.write(content);
+      res.end();
+    });
   } else if (req.url === '/route_1') {
-    // Respond with correct output for big text
-    res.write('Correct output - big text');
-    res.end();
+    // Fetch content from the specified URL for big text
+    fetchContent('http://loripsum.net/api/100/long', (content) => {
+      res.write(content);
+      res.end();
+    });
   } else if (req.url === '/route_2') {
     // Respond with correct output for empty text
     res.write('Correct output - empty text');
@@ -29,32 +33,18 @@ server.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
 
-if (process.argv.length !== 4) {
-  console.error('Usage: node 3-request_store.js <URL> <outputFilePath>');
-  process.exit(1);
+function fetchContent(url, callback) {
+  request(url, { encoding: 'utf-8' }, (error, response, body) => {
+    if (error) {
+      console.error('Error making the request:', error);
+      process.exit(1);
+    }
+
+    if (response.statusCode !== 200) {
+      console.error('Failed to fetch data from the URL. Status code:', response.statusCode);
+      process.exit(1);
+    }
+
+    callback(body);
+  });
 }
-
-const url = process.argv[2];
-const outputPath = process.argv[3];
-
-request(url, { encoding: 'utf-8' }, (error, response, body) => {
-  if (error) {
-    console.error('Error making the request:', error);
-    process.exit(1);
-  }
-
-  if (response.statusCode !== 200) {
-    console.error('Failed to fetch data from the URL. Status code:', response.statusCode);
-    process.exit(1);
-  }
-
-  try {
-    fs.writeFileSync(outputPath, body, 'utf-8');
-    console.log(`Content successfully stored in ${outputPath}`);
-    // Stop the server after successfully storing content
-    server.close();
-  } catch (writeError) {
-    console.error('Error writing to the file:', writeError);
-    process.exit(1);
-  }
-});
