92f42a4d9920df60f952732477ca9e4acdc048a5
GustaveIR <iraguhagustave@gmail.com>
2024-01-25 18:12:22 +0200

    javascript-network

--- javascript-network/3-request_store.js
+++ javascript-network/3-request_store.js
@@ -1,54 +1,32 @@
 const request = require('request');
 const fs = require('fs');
-const http = require('http');
 
-function getRandomPort() {
-  return Math.floor(Math.random() * (65535 - 49152 + 1)) + 49152;
+if (process.argv.length !== 4) {
+  console.error('Usage: node 3-request_store.js <URL> <FilePath>');
+  process.exit(1);
 }
 
-const server = http.createServer((req, res) => {
-  if (req.url === '/route_0') {
-    // Fetch content from the specified URL for small text
-    fetchContent('http://loripsum.net/api', (content) => {
-      res.write(content);
-      res.end();
-    });
-  } else if (req.url === '/route_1') {
-    // Fetch content from the specified URL for big text
-    fetchContent('http://loripsum.net/api/100/long', (content) => {
-      res.write(content);
-      res.end();
-    });
-  } else if (req.url === '/route_2') {
-    // Respond with correct output for empty text
-    res.write('Correct output - empty text');
-    res.end();
-  } else {
-    // Handle invalid routes
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.write('Invalid route');
-    res.end();
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+request(url, { encoding: 'utf-8' }, (error, response, body) => {
+  if (error) {
+    console.error('Error making the request:', error);
+    process.exit(1);
   }
-});
 
-const PORT = getRandomPort();
+  if (response.statusCode !== 200) {
+    console.error('Failed to fetch data from the URL. Status code:', response.statusCode);
+    process.exit(1);
+  }
 
-server.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
-});
-
-function fetchContent(url, callback) {
-  request(url, { encoding: 'utf-8' }, (error, response, body) => {
-    if (error) {
-      console.error('Error making the request:', error);
+  // Write the body content to the specified file
+  fs.writeFile(filePath, body, 'utf-8', (err) => {
+    if (err) {
+      console.error('Error writing to the file:', err);
       process.exit(1);
     }
 
-    if (response.statusCode !== 200) {
-      console.error('Failed to fetch data from the URL. Status code:', response.statusCode);
-      process.exit(1);
-    }
-
-    callback(body);
+    console.log(`Content successfully written to ${filePath}`);
   });
-}
+});
