d1a0d288c606b9b8db9b035ed519d003eeeb56cb
GustaveIR <iraguhagustave@gmail.com>
2024-01-31 19:00:46 +0200

    javascript-network

--- javascript-network/3-request_store.js
+++ javascript-network/3-request_store.js
@@ -1,28 +1,25 @@
 const axios = require('axios');
 const fs = require('fs');
 
-if (process.argv.length < 4) {
-  console.error('Usage: node 3-request_store.js <URL1> <URL2> ... <URLn> <FilePath>');
+if (process.argv.length !== 4) {
+  console.error('Usage: node 3-request_store.js <URL> <FilePath>');
   process.exit(1);
 }
 
-const filePath = process.argv[process.argv.length - 1];
-const urls = process.argv.slice(2, -1);
+const url = process.argv[2];
+const filePath = process.argv[3];
 
-const processURL = async (url) => {
+const fetchDataAndStoreToFile = async () => {
   try {
     const response = await axios.get(url, { responseType: 'text' });
 
-    // Append the body content to the specified file
-    fs.appendFileSync(filePath, response.data + '\n', 'utf-8');
+    // Write the body content to the specified file
+    fs.writeFileSync(filePath, response.data, 'utf-8');
 
-    console.log(`Content successfully written to ${filePath} for ${url}`);
+    console.log(`Content successfully written to ${filePath}`);
   } catch (error) {
-    console.error(`Error making the request for ${url}:`, error.message);
+    console.error(`Error making the request:`, error.message);
   }
 };
 
-// Process each URL
-urls.forEach((url) => {
-  processURL(url);
-});
+fetchDataAndStoreToFile();
