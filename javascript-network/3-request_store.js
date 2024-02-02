456658a6618b349df3b65721325d8f4a84632956
GustaveIR <iraguhagustave@gmail.com>
2024-02-02 15:46:48 +0200

    javascript-network

--- javascript-network/3-request_store.js
+++ javascript-network/3-request_store.js
@@ -1,5 +1,5 @@
 const axios = require('axios');
-const fs = require('fs');
+const fs = require('fs/promises');
 
 if (process.argv.length !== 3) {
   console.error('Usage: node 3-request_store.js <FilePath>');
@@ -17,17 +17,22 @@ const urls = [
 const processURL = async (url) => {
   try {
     const response = await axios.get(url);
-    fs.appendFileSync(filePath, response.data, 'utf-8');
+    await fs.appendFile(filePath, response.data, 'utf-8');
     console.log(`Content successfully written to ${filePath} for ${url}`);
   } catch (error) {
     console.error(`Error making the request for ${url}:`, error.message);
   }
 };
 
-// Process each URL
-for (const url of urls) {
-  await processURL(url);
-}
+// Process each URL asynchronously
+const processAllURLs = async () => {
+  for (const url of urls) {
+    await processURL(url);
+  }
 
-// Add a newline at the end of the file
-fs.appendFileSync(filePath, '\n', 'utf-8');
+  // Add a newline at the end of the file
+  await fs.appendFile(filePath, '\n', 'utf-8');
+};
+
+// Call the asynchronous function
+processAllURLs();
