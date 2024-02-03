a9e48e4a676abad7ac0a34cfc35bcee3ca27b220
GustaveIR <iraguhagustave@gmail.com>
2024-02-03 09:16:57 +0200

    javascript-network

---
+++ javascript-network/4-completed_tasks.js
@@ -0,0 +1,36 @@
+const request = require('request');
+
+const apiUrl = process.argv[2];
+
+if (!apiUrl) {
+  console.error('Please provide the API URL as the first argument.');
+  process.exit(1);
+}
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error('Error making request:', error);
+    process.exit(1);
+  }
+
+  if (response.statusCode !== 200) {
+    console.error('Unexpected status code:', response.statusCode);
+    process.exit(1);
+  }
+
+  const todos = JSON.parse(body);
+
+  const completedTasksByUser = {};
+
+  todos.forEach((todo) => {
+    if (todo.completed) {
+      if (completedTasksByUser[todo.userId]) {
+        completedTasksByUser[todo.userId]++;
+      } else {
+        completedTasksByUser[todo.userId] = 1;
+      }
+    }
+  });
+
+  console.log(completedTasksByUser);
+});
